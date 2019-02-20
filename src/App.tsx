d537a67f7215041012df2a83d9a8cd3dcf24200f
Debian live <eee1@i.ua>
2019-02-20 16:33:14 +0200

    ext comp access, wrong, ver 0.1.2 (J2K)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const sVersion = "ver 0.1.1 (J2K)";
+const sVersion = "ver 0.1.2 (J2K)";
 
 export class Main extends Component {
   render() {
@@ -29,13 +29,20 @@ interface IProps {
   height_p?: string
 }
 export class TextAreas extends React.Component<IProps, IState> {
+  state = {text_s: "hello to Mx\n", height_s: '250', width_s: '580'};
+  sFile = this.props.file_p || '2.txt'
   constructor(props: IProps) {
     super(props);
-    const sFile = props.file_p || '1.txt'
-    this.sGetData(location.href + '/data/' + sFile);
+    //const sFile = props.file_p || '1.txt'
+    this.sGetData(location.href + '/data/' + this.sFile);
     this.handleTextChange = this.handleTextChange.bind(this);
-    this.state = {text_s: "hello to Mx\n", height_s: '250', width_s: '580'};
+    // this.state = {text_s: "hello to Mx\n", height_s: '250', width_s: '580'};
   }
+  setCompState() {
+    console.log("inner func...");
+    this.setState({'text_s': "asdjfl !!!"})
+  }
+
   sGetData = async (path: string) => {
     console.log("async: " + path)
     try {
@@ -48,9 +55,11 @@ export class TextAreas extends React.Component<IProps, IState> {
       console.error(error);
     }
   }
+
   handleTextChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     this.setState({text_s: e.target.value});
   }
+
   render() {
     const taStyle = {
       height: this.state.height_s+'px',
@@ -59,20 +68,20 @@ export class TextAreas extends React.Component<IProps, IState> {
       verticalAlign: "top",
     };
     return (
-      <div>
+      <>
         <hr />
         <p>In the textarea symbols: [{this.state.text_s.length}]</p>
         <hr />
         <fieldset>
-          <legend>Enter text:</legend>
+          <legend>4 Enter text:</legend>
           <div style={{border: "0px solid", verticalAlign: "top"}}>
           <textarea style={taStyle} className="_halfsize" value={this.state.text_s}
               onChange={this.handleTextChange}
           />
           </div>
         </fieldset>
-        {"===>"}<br />{this.state.text_s}<br />{"<==="}
-      </div>
+        {"===>"}<br /><h4>{this.state.text_s}</h4><br />{"<==="}
+      </>
     );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,11 @@ import * as App from './App';
 
 ReactDOM.render(<App.Main />, document.getElementById('root'));
 ReactDOM.render(<App.TextAreas />, document.getElementById('textareas'));
-const compTA2 = <App.TextAreas file_p = "2.txt"/>
-ReactDOM.render(compTA2, document.getElementById('textareas2'));
+const compTA2 = <App.TextAreas file_p = "2.txt" />
+const oTA2: any = ReactDOM.render(compTA2, document.getElementById('textareas2'));
+console.log(oTA2);
+oTA2.setCompState();
+oTA2.render();
+//oTA2.setState({text_s: "sdfsdf"});
+
+//compTA2.setCompState() //.setState({text_s: "hello TA 2", height_s: '50', width_s: '70'})
